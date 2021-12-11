4a507e8635d75d4b077bf3bd2dac1938710bc687
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2021-12-11 10:26:10 +0000

    Named pet

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,4 +4,8 @@ describe('constructor', () => {
     it('returns an object', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
+    it('sets the name property', () => {
+      const pet = new Pet('Jessie');
+      expect(pet.name).toEqual('Jessie');
+    });
   });
