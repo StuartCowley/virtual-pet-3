8fa2eecd39432adb1c052b57b4dd1a244753ba29
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2021-12-11 10:36:20 +0000

    added age and incrementation of age

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -2,10 +2,22 @@ const Pet = require('../src/pet');
 
 describe('constructor', () => {
     it('returns an object', () => {
-      expect(new Pet('Fido')).toBeInstanceOf(Object);
+      expect(new Pet('Jessie')).toBeInstanceOf(Object);
     });
     it('sets the name property', () => {
       const pet = new Pet('Jessie');
       expect(pet.name).toEqual('Jessie');
     });
+    it('has an initial age of 0', () => {
+      const pet = new Pet('Jessie');
+      expect(pet.age).toEqual(0);
+    });
+  });
+
+  describe('growUp', () => {
+    it('increments the age by 1', () => {
+      const pet = new Pet('Jessie');
+      pet.growUp();
+      expect(pet.age).toEqual(1);
+    })
   });
