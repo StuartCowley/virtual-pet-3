036fee9c2b971534e2061ea5106a3a1ecbb34d71
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2021-12-15 18:21:32 +0000

    tidied up test with beforeEach

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,54 +1,57 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
+  let pet;
+  beforeEach(() => {
+   pet = new Pet('Jessie');
+  });
     it('returns an object', () => {
       expect(new Pet('Jessie')).toBeInstanceOf(Object);
     });
     it('sets the name property', () => {
-      const pet = new Pet('Jessie');
       expect(pet.name).toEqual('Jessie');
     });
     it('has an initial age of 0', () => {
-      const pet = new Pet('Jessie');
       expect(pet.age).toEqual(0);
     });
     it('has an initial hunger of 0', () => {
-      const pet = new Pet('Jessie');
      expect(pet.hunger).toEqual(0);
     });
     it('has initial fitness of 10', () => {
-      const pet = new Pet('Jessie');
       expect(pet.fitness).toEqual(10);
     } )
   });
 
   describe('growUp', () => {
+    let pet;
+    beforeEach(() => {
+     pet = new Pet('Jessie');
+    });
     it('increments the age by 1', () => {
-      const pet = new Pet('Jessie');
       pet.growUp();
       expect(pet.age).toEqual(1);
     });
     it('increments the hunger by 5', () => {
-      const pet = new Pet('Jessie');
       pet.growUp();
       expect(pet.hunger).toEqual(5);
     });
     it('decreases the fitness by 3', () => {
-      const pet = new Pet('Jessie')
       pet.growUp();
       expect(pet.fitness).toEqual(7);
     });
   });
 
   describe('walk', () => {
+    let pet;
+    beforeEach(() => {
+     pet = new Pet('Jessie');
+    });
     it('increments the fitness by 4', () => {
-      const pet = new Pet('Jessie');
       pet.fitness = 4;
       pet.walk();
       expect(pet.fitness).toEqual(8);
     });
     it('increases fitness to a maximum of 10', () => {
-      const pet = new Pet('Jessie');
       pet.fitness = 8;
       pet.walk();
       expect(pet.fitness).toEqual(10);
@@ -56,14 +59,16 @@ describe('constructor', () => {
   });
 
   describe('feed', () => {
+    let pet;
+    beforeEach(() => {
+     pet = new Pet('Jessie');
+    });
     it('decreases hunger by 3', () => {
-      const pet = new Pet('Jessie');
       pet.hunger = 5;
       pet.feed();
       expect(pet.hunger).toEqual(2);
     });
     it('decreases hunger to a minimum of 0', () => {
-      const pet = new Pet('Jessie');
       pet.hunger = 2;
       pet.feed();
       expect(pet.hunger).toEqual(0);
